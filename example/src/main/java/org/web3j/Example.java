682c3eeb84881ca98be90b935dfaa0d60e0ffbc3
Xin <xw@zkplab.com>
2020-08-01 17:13:18 -0700

    Fix lint.

--- example/src/main/java/org/web3j/Example.java
+++ example/src/main/java/org/web3j/Example.java
@@ -12,25 +12,16 @@
  */
 package org.web3j;
 
-import java.io.File;
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
-import java.util.stream.Collectors;
-
-
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
-import io.reactivex.Single;
 import io.reactivex.disposables.Disposable;
 
-
-import org.web3j.crypto.ECKeyPair;
 import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
 import org.web3j.crypto.Keys;
 import org.web3j.crypto.RawTransaction;
 import org.web3j.crypto.TransactionEncoder;
@@ -43,14 +34,9 @@ import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.protocol.core.methods.response.EthTransaction;
 import org.web3j.protocol.core.methods.response.Transaction;
 import org.web3j.protocol.http.HttpService;
-
-import org.web3j.utils.Convert;
 import org.web3j.utils.Numeric;
 
-
-/**
- * Demonstrations of Origo Java SDK.
- */
+/** Demonstrations of Origo Java SDK. */
 public class Example {
 
     private static final int COUNT = 1;
@@ -64,7 +50,6 @@ public class Example {
 
     private void run() throws Exception {
 
-
         System.out.println("Doing clientVersionExample");
         clientVersionExample();
 
@@ -74,18 +59,26 @@ public class Example {
         generateAddressExample();
         // createAndSignTransactionExample();
         getLatestBlockSubscriptionExample();
-        System.exit(0);  // we explicitly call the exit to clean up our ScheduledThreadPoolExecutor used by web3j
+        System.exit(
+                0); // we explicitly call the exit to clean up our ScheduledThreadPoolExecutor used
+        // by web3j
     }
 
     public static void main(String[] args) throws Exception {
         new Example().run();
-
     }
+
     void getLatestBlockSubscriptionExample() throws Exception {
-        Disposable subscription = web3j.blockFlowable(true).subscribe(block -> {
-            System.out.println("Sweet, block number " + block.getBlock().getNumber()
-                    + " has just been created");
-        }, Throwable::printStackTrace);
+        Disposable subscription =
+                web3j.blockFlowable(true)
+                        .subscribe(
+                                block -> {
+                                    System.out.println(
+                                            "Sweet, block number "
+                                                    + block.getBlock().getNumber()
+                                                    + " has just been created");
+                                },
+                                Throwable::printStackTrace);
 
         TimeUnit.MINUTES.sleep(2);
         subscription.dispose();
@@ -94,10 +87,16 @@ public class Example {
     void clientVersionExample() throws Exception {
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        Disposable subscription = web3j.web3ClientVersion().flowable().subscribe(x -> {
-            System.out.println("Client is running version: " + x.getWeb3ClientVersion());
-            countDownLatch.countDown();
-        });
+        Disposable subscription =
+                web3j.web3ClientVersion()
+                        .flowable()
+                        .subscribe(
+                                x -> {
+                                    System.out.println(
+                                            "Client is running version: "
+                                                    + x.getWeb3ClientVersion());
+                                    countDownLatch.countDown();
+                                });
 
         countDownLatch.await();
         subscription.dispose();
@@ -107,25 +106,36 @@ public class Example {
         System.out.println("Doing getTransactionExample");
 
         EthTransaction ethTransaction =
-                web3j.ethGetTransactionByHash("0x2a4520eaa85fec4bfffae7e30da1b7d4f9e06a5bd21560ae8cf87bb65de64974").send();
+                web3j.ethGetTransactionByHash(
+                                "0x2a4520eaa85fec4bfffae7e30da1b7d4f9e06a5bd21560ae8cf87bb65de64974")
+                        .send();
         Transaction transaction = ethTransaction.getTransaction().get();
 
-        System.out.println("Transaction: " + transaction.getHash() + " , from: " +
-                transaction.getOgoFrom() + " , to: " + transaction.getOgoTo() + "\n");
+        System.out.println(
+                "Transaction: "
+                        + transaction.getHash()
+                        + " , from: "
+                        + transaction.getOgoFrom()
+                        + " , to: "
+                        + transaction.getOgoTo()
+                        + "\n");
     }
 
     void getBlockExample() throws Exception {
         System.out.println("Doing getBlockExample");
         EthBlock ethBlock =
-                web3j.ethGetBlockByNumber(DefaultBlockParameter.valueOf(Numeric.toBigInt("0xA5624")), true).send();
+                web3j.ethGetBlockByNumber(
+                                DefaultBlockParameter.valueOf(Numeric.toBigInt("0xA5624")), true)
+                        .send();
         EthBlock.Block block = ethBlock.getBlock();
-        List<Transaction>  transactions = block.getTransactions().stream()
-                .map(transactionResult -> (Transaction) transactionResult.get())
-                .collect(Collectors.toList());
-        System.out.println("block " + block.getNumber() + ", has " + transactions.size() + " transactions.\n");
+        List<Transaction> transactions =
+                block.getTransactions().stream()
+                        .map(transactionResult -> (Transaction) transactionResult.get())
+                        .collect(Collectors.toList());
+        System.out.println(
+                "block " + block.getNumber() + ", has " + transactions.size() + " transactions.\n");
         System.out.println("Transaction 0 is: " + transactions.get(0).getHash());
     }
-
 
     void generateAddressExample() throws Exception {
         ECKeyPair ecKeyPair = Keys.createEcKeyPair();
@@ -133,37 +143,45 @@ public class Example {
 
         String sPrivatekeyInHex = privateKeyInDec.toString(16);
 
-        Credentials credential  = Credentials.create(ecKeyPair);
+        Credentials credential = Credentials.create(ecKeyPair);
         String address = credential.getAddress();
         String addressOgo = credential.getOgoAddress();
 
-        System.out.println("private key: " + sPrivatekeyInHex + " address:" + address + ", ogo address:" + addressOgo);
+        System.out.println(
+                "private key: "
+                        + sPrivatekeyInHex
+                        + " address:"
+                        + address
+                        + ", ogo address:"
+                        + addressOgo);
     }
 
-    void createAndSignTransactionExample() throws Exception{
+    void createAndSignTransactionExample() throws Exception {
         String privateKey = "CHANGE TO YOUR PRIVATE KEY.";
         Credentials credentials = Credentials.create(privateKey);
 
         // get the next available nonce
-        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
-                credentials.getAddress(), DefaultBlockParameterName.LATEST).sendAsync().get();
+        EthGetTransactionCount ethGetTransactionCount =
+                web3j.ethGetTransactionCount(
+                                credentials.getAddress(), DefaultBlockParameterName.LATEST)
+                        .sendAsync()
+                        .get();
         BigInteger nonce = ethGetTransactionCount.getTransactionCount();
         System.out.println("nonce: " + nonce);
 
         // create our transaction
-        RawTransaction rawTransaction  = RawTransaction.createOgoTransaction(
-                nonce,
-                BigInteger.valueOf(1000000000),
-                BigInteger.valueOf(21000),
-                "ogopub19sygy30jgqm8wyms38lfqtn692dkaje946ye8u",
-                BigInteger.ONE);
+        RawTransaction rawTransaction =
+                RawTransaction.createOgoTransaction(
+                        nonce,
+                        BigInteger.valueOf(1000000000),
+                        BigInteger.valueOf(21000),
+                        "ogopub19sygy30jgqm8wyms38lfqtn692dkaje946ye8u",
+                        BigInteger.ONE);
         System.out.println(rawTransaction.getTo());
         byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
         String hexValue = Numeric.toHexString(signedMessage);
         System.out.println("Transaction message: " + hexValue);
-        EthSendTransaction transactionResponse =
-                web3j.ethSendRawTransaction(hexValue).send();
+        EthSendTransaction transactionResponse = web3j.ethSendRawTransaction(hexValue).send();
         System.out.println("Sent transaction: " + transactionResponse.getTransactionHash());
     }
-
 }
